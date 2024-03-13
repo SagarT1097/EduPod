a56db531376c037e76eb80bd30176beb7cafbdec
Sagar Tambrahalli <sagar.uu97@gmail.com>
2024-03-13 22:03:13 +0100

    Added Favorites func

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -4,6 +4,7 @@ import { Song } from "@/types";
 import MediaItem from "@/components/MediaItem";
 //import LikeButton from "@/components/LikeButton";
 import useOnPlay from "@/hooks/useOnPlay";
+import LikeButton from "@/components/LikeButton";
 
 interface SearchContentProps {
   songs: Song[];
@@ -36,7 +37,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
           <div className="flex-1">
             <MediaItem onClick={(id: string) => onPlay(id)} data={song} />
           </div>
-          {/* <LikeButton songId={song.id} /> */}
+          <LikeButton songId={song.id} />
         </div>
       ))}
     </div>
